bfa5ff73141c72bd89b1d91a015361133a2bdffc
alinamur2015ekb-arch <alinamur2015ekb@gmail.com>
2026-06-16 23:11:13 +0500

    Refactor my_command.py for improved clarity

--- main/my_command.py
+++ main/my_command.py
@@ -2,7 +2,7 @@ from aiogram import Router, F
 from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
 from database import get_answer_by_id, get_play_by_id
-from aiogram.enmus import ParseMode
+from aiogram.enums import ParseMode
 
 router = Router()
 
