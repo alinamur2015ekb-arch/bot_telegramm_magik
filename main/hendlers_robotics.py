582273c2393119c4624ebd3a88986d42cf8976ed
alinamur2015ekb-arch <alinamur2015ekb@gmail.com>
2026-06-16 23:04:48 +0500

    Update hendlers_robotics.py

--- main/hendlers_robotics.py
+++ main/hendlers_robotics.py
@@ -80,7 +80,11 @@ async def c6(message: Message, state: FSMContext):
     
     await message.answer(f"У вас {one_count_robotics}/3 <b>правильных</b> ответов", parse_mode="HTML")
     
-    await create_answer(answer_robotics1=one_count_robotics, message.from_user.id)
+    await create_answer(
+    answer_math1=count_math1,
+    user_id=message.from_user.id
+)
+
     await state.clear()
 
 
@@ -146,5 +150,9 @@ async def c7(message: Message, state: FSMContext):
     
     await message.answer(f"У вас {two_count_robotics}/3 <b>правильных</b> ответов", parse_mode="HTML")
     
-    await create_answer(answer_robotics2=two_count_robotics, message.from_user.id)
+    await create_answer(
+    answer_math1=count_math1,
+    user_id=message.from_user.id
+)
+
     await state.clear()
