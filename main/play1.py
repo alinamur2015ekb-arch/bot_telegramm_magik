11bc15ce89adabc42f19b0d244e02ba41a015ecc
alinamur2015ekb-arch <alinamur2015ekb@gmail.com>
2026-06-25 22:26:57 +0500

    Refactor play1.py for clarity and organization

--- main/play1.py
+++ main/play1.py
@@ -128,7 +128,7 @@ async def mems5(message: Message, state: FSMContext):
     await message.answer(f"У вас {count_mems}/5 правильных ответов😝")
 
     await create_answer(
-        mem=count_mems,
+        meme=count_mems,
         user_id=message.from_user.id
 )
   
